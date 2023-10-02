2c6215e69a35d500ca6b53e10fbb6df4ee9f7e79
buihuutrii <21130575@st.hcmuaf.edu.vn>
2023-10-02 22:39:50 +0700

    agent

---
+++ OneDrive/AI_HD301/lab1/src/agent_AB/AgentProgram.java
@@ -0,0 +1,17 @@
+package agent_AB;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		//TODO
+		if(p.getLocationState()==p.getLocationState().DIRTY) {
+			return Environment.SUCK_DIRT;
+		}else if (p.getAgentLocation().equals(Environment.LOCATION_A)) {
+			return Environment.MOVE_RIGHT;
+		}else if (p.getAgentLocation().equals(Environment.LOCATION_B)) {
+			return Environment.MOVE_LEFT;
+		}
+		return NoOpAction.NO_OP;
+		
+	}
+}
